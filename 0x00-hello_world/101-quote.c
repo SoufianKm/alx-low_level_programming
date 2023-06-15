7a18a016a70a1e8082b20667af3668904a8cdc69
SoufianKm <soufian.kamma@gmail.com>
2023-06-15 10:21:14 -0700

    Answer to 'UNIX is basically a simple...' task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * print a text without using printf or puts function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, text, sizeof(text) - 1);
+	return (1);
+}
