dd3f5e20414ee414039b83ffbb28f6dbda3cbec8
SoufianKm <soufian.kamma@gmail.com>
2023-06-16 14:18:35 -0700

    Handle the last digit of negative number given

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	last_digit = (n % 10);
+	if (last_digit < 0)
+		last_digit *= -1;
 	if (last_digit  == 0)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else if (last_digit > 5)
