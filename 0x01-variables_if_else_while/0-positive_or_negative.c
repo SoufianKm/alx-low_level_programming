c8c7d89eb52da92ec914a94d8c38105e45fc8fc3
SoufianKm <soufian.kamma@gmail.com>
2023-06-16 13:05:54 -0700

    Applied betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ *
+ * This function should print the number 
+ * with is negative or positive or equal to 0
+ *
+ * Return : Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -10,11 +17,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n == 0)
-		printf("%d is zero\n",n);
-	else if(n < 0)	
-		printf("%d is negative\n",n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	return (0);
 }
