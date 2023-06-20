21461755d8b3ec97195f9a166cc65aa32e91b167
SoufianKm <soufian.kamma@gmail.com>
2023-06-20 05:40:55 -0700

    Answered to '_putchar' task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * This program should print "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(1,"_putchar",strln("_putchar"));
+	return (0);
+}
