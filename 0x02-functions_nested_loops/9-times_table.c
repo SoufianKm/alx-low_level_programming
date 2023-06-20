9109b598ae5d7095b0d66cc2b2f7bd3376267051
SoufianKm <soufian.kamma@gmail.com>
2023-06-20 15:36:49 -0700

    Correct 'Times table' answer task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,10 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			res = i * j;
-			if (res < 10)
+			if (j == 0)
+			{
+                                _putchar('0' + res);
+			} else if (res < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
