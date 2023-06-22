2d57cd0c63b3ce4b1fbe107d61d1add504e6d421
SoufianKm <soufian.kamma@gmail.com>
2023-06-22 06:10:47 -0700

    Answer to 'Leap year' task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,10 @@
 * Return: void
 */
 
+04 + 90 = 94 | 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
 		if (month >= 2 && day >= 60)
 		{
