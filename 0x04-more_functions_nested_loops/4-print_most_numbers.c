d0065f89ff93de2d9443a1c28ddd5da0fb9bfbf5
SoufianKm <soufian.kamma@gmail.com>
2023-06-22 13:30:10 -0700

    Answer to 'I believe in numbers and signs' task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,10 @@ void print_most_numbers(void)
 {
 	int n = 0;
 
-	while(n < 10)
+	while(n++)
 	{
-		if (n != 2 || n != 4)
+		if (n != 2 && n != 4)
 			_putchar('0' + n);
-		n++;
 	}
 	_putchar('\n');
 }
