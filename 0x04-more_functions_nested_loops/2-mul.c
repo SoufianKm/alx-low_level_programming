080752d797b85d1584c18f2271acc4994860fe7c
SoufianKm <soufian.kamma@gmail.com>
2023-06-22 11:12:21 -0700

    Answer to 'Multiplies' task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - mutliplies two integes
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: multiplies of paramerters
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
