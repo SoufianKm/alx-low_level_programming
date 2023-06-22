40e9775f4ce60b8a2d649d87bce0bf1430f197a7
SoufianKm <soufian.kamma@gmail.com>
2023-06-22 10:30:49 -0700

    Answer to 'isdigit' task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: parameter given 
+ * Return: 1 if c an uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	return (0);
+}
