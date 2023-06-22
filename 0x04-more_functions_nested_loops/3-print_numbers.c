e907882c4a1305b6a4399cfb6d8f5a903c30a86b
SoufianKm <soufian.kamma@gmail.com>
2023-06-22 13:09:27 -0700

    Answer to 'Print number from 0 to 9' task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print number from 0 to 9
+ * using _putchar
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while(n < 10)
+	{
+		_putchar('0' + n);
+		n++;
+	}
+}
