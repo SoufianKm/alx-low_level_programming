b8d896aa2def6c1b760a08f318818491b735b6de
SoufianKm <soufian.kamma@gmail.com>
2023-06-23 16:05:41 -0700

    Answer to 'The first 98 fibonacci' task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,18 +11,18 @@
  */
 int main(void)
 {
-	long previous_nbr = 0, current_nbr = 1, res;
+	unsigned long previous_nbr = 0, current_nbr = 1, res;
 	int counter = 0;
 
 	while (counter <= 98)
 	{
 		res = previous_nbr + current_nbr;
-	
+
 		if (res < 0)
 			printf("%ld", res * (-1));
 		else
 			printf("%ld", res);
-		
+
 		if (counter <= 97)
 			printf(", ");
 
