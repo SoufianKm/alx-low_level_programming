8521a0ffbc18ddc372b26f72318635ccba513fc2
SoufianKm <soufian.kamma@gmail.com>
2023-06-22 18:08:25 -0700

    Answer to 'Print triangle' task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,15 +17,17 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 1; j <= size; j++)
 			{
-				if (j < size - i)
+				if (j <= size - i)
 				{
 					_putchar(' ');
 				} else
+				{
 					_putchar('#');
+				}
 			}
 			_putchar('\n');
 		}
