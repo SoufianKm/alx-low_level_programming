6da4d4e45a7a5dc30bbe3df75806924288b95f2b
SoufianKm <soufian.kamma@gmail.com>
2023-06-23 11:05:16 -0700

    Answer to 'Largest prime factor' task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,20 +12,20 @@ int main(void)
 {
 	int prime_factor = 0, i = 2;
 	long n = 612852475143;
-	
+
 	do {
 		if (n % i == 0)
 			n /= i;
-		
+
 		if (i > prime_factor)
 			prime_factor = i;
-		
+
 		if (n % i == 0)
 			i = 2;
 		else
 			i++;
 	} while (n > 1);
-	
+
 	printf("%d\n", prime_factor);
 
 	return (0);
