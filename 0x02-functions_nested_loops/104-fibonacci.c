17ab0246b99d259ead5a3f253f9c1f1e4893f7eb
SoufianKm <soufian.kamma@gmail.com>
2023-06-23 15:46:02 -0700

    Answer to 'The first 98 fibonacci' task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * This program should print
+ * the first 98 fibonacci sequence number
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	long previous_nbr = 0, current_nbr = 1, res;
+	int counter = 0;
+
+	while (counter < 98)
+	{
+		res = previous_nbr + current_nbr;
+		if (res < 0)
+			res *= (-1);
+		printf("%ld", res);
+
+		if (counter < 97)
+			printf(", ");
+
+		previous_nbr = current_nbr;
+		current_nbr = res;
+		counter++;
+	}
+	printf("\n");
+
+	return (0);
+}
