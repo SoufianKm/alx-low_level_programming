038752e2d81186fcb864b8bc0c093bc5a8270ff3
SoufianKm <soufian.kamma@gmail.com>
2023-06-23 11:27:14 -0700

    Answer to 'Print integer number' task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - print a number
+ * @n: integer parameter
+ */
+
+void print_number(int n)
+{
+	if (n >= 10)
+	{
+		n /= 10;
+		print_number(n);
+	}
+	_putchar('0' + n);
+}
