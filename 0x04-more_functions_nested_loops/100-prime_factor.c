d9a3cdc9793205a43ca79e7e8d2a303ffd1b6551
SoufianKm <soufian.kamma@gmail.com>
2023-06-23 03:53:29 -0700

    Answer to 'Largest premium factor' task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program should find and print
+ * the largest prime factor of a number
+ *
+ * Return: 0 Always (Success)
+ */
+int main (void)
+{
+	int p_f = 0, i = 2;
+       	long n = 612852475143;
+	
+	do
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+		}
+	
+		if (i > p_f)
+		{
+			p_f = i;
+		}
+		if (n % i == 0)
+		{
+			i = 2;
+		}
+		else
+		{
+			i++;
+		}
+	} while (n > 1);
+	printf("(%d)\n", p_f);
+	
+	return (0);
+}
