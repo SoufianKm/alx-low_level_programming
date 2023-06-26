dff34c0b88f803ad9a97fd842553ca2a82b3b0ee
SoufianKm <soufian.kamma@gmail.com>
2023-06-26 11:44:37 -0700

    Answer to 'Length of string' task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - calculate the length of a string
+ * @s : pointer string parameter
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
