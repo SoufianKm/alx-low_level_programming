a0529e5182d8f77117f21b14ea8ab43cde00d201
SoufianKm <soufian.kamma@gmail.com>
2023-06-26 11:06:23 -0700

    Answer to '98 Battery st' task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,9 +4,8 @@
 /**
  * reset_to_98 - update the variable pointed to, with the value 98
  *
- * @*n: integer pointer parameter
+ * @n: integer pointer parameter
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
