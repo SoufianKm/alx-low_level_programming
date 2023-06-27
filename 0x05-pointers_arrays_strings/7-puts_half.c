3d862e586067b37d1c6c4bae4ead03de1cffc029
SoufianKm <soufian.kamma@gmail.com>
2023-06-27 03:35:21 -0700

    Answer to 'Puts half' task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,18 +3,19 @@
 #include <string.h>
 
 /**
- * puts_half - print a half string given
- * @s : pointer char parameter
+ * puts_half - print a seconde half of string given
+ * @str : pointer char parameter
  */
 void puts_half(char *str)
 {
-	int i, l = strlen(str), counter = 0;
-	
+	int i, counter = 0;
+	long l = strlen(str);
+
 	if (l % 2 == 0)
 		counter = l / 2;
 	else
-		counter = l - 1 / 2;
-	
+		counter = (l - 1) / 2;
+
 	for (i = counter; i < l ;i++)
 			_putchar(str[i]);
 	_putchar('\n');
