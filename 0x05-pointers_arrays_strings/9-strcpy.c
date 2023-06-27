2cc1b59ad004983d9c9a5b882f9f5491b2d8adf4
SoufianKm <soufian.kamma@gmail.com>
2023-06-27 04:41:48 -0700

    Answer to 'Strcpy' task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,6 +5,7 @@
  * _strcpy -cpoy string pointed by src
  * @dest : pointer char parameter
  * @src : pointer char parameter
+ * Return : value pointed to dest
  */
 char *_strcpy(char *dest, char *src)
 {
