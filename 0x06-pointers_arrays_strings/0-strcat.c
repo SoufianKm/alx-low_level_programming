876331e162f4cef866fc35963e61ae6b495b935b
SoufianKm <soufian.kamma@gmail.com>
2023-06-29 09:13:24 -0700

    Answer to 'strcat' task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concantenates two strings
+ * @dest: pointer char parameter
+ * @src: pointer char parameter
+ * Return: resulting string dest
+ */
+char _strcat(char *dest, char *src)
+{
+	return (strcat(dest,src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#This directory is about learning more pointers, arrays and strings.
