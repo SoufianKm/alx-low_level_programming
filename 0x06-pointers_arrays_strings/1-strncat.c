2112858c6cc4b8b88d1b33e7475251654c3acdb6
SoufianKm <soufian.kamma@gmail.com>
2023-06-29 09:28:29 -0700

    Answer to 'strncat' task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concantenates two strings
+ * using at most n bytes from src
+ * @dest: pointer char parameter
+ * @src: pointer char parameter
+ * Return: resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	dest = strncat(dest, src, n);
+	return (dest);
+}
