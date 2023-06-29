ffe82d51760d1b79268f41360d4359357fcf4c31
SoufianKm <soufian.kamma@gmail.com>
2023-06-29 09:45:22 -0700

    Answer to 'strcmp' task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: pointer char parameter
+ * @s2: pointer char parameter
+ * Return: resulting string dest
+ */
+char *_strcmp(char *s1, char *s2)
+{
+	dest = strcmp(s1, s2);
+	return (dest);
+}
