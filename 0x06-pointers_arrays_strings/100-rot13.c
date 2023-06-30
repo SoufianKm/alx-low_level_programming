33937c786b0e23f4ab14b8f4214025106e82e8bc
SoufianKm <soufian.kamma@gmail.com>
2023-06-30 09:47:41 -0700

    Answer to 'rot13' task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - incode characters using rot13
+ * @str: pointer char parameter
+ * Return: string encoded
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char l[] = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char r[] = "nNoOpPqQrRsStTuUvVwWxXyYzZ";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (str[i] == l[j])
+			{
+				str[i] = r[j];
+			}
+		}
+	}
+
+	return (str);
+}
