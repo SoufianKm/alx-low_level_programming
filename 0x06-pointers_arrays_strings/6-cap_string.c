cfbd802e48e71eb624953d12aec3400e461e5039
SoufianKm <soufian.kamma@gmail.com>
2023-06-30 07:44:23 -0700

    Answer to 'capitalize on what comes' task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: pointer char parameter
+ * Return: all words of string capitalised
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j = 0, k = 0;
+	char sp[] = {',',';','.','!','?','\"','(',')','{','}','\t','\n',' '};
+
+	while (str[i] != '\0')
+	{
+		while (sp[j] != '\0')
+		{
+			if (str[i] == sp[j])
+			{
+				if (str[i + 1] == ' ')
+					k = i + 2;
+				else
+					k = i + 1;
+
+				if (str[k] >= 'a' && str[k] <= 'z')
+					str[k] -= 32;
+				break;
+			} else
+				j++;
+		}
+		j = 0;
+		i++;
+	}
+
+	return (str);
+}
