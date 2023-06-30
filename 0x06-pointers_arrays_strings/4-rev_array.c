a46581a288993721807ec55b2c208155e808350e
SoufianKm <soufian.kamma@gmail.com>
2023-06-30 03:39:56 -0700

    Answer to 'Reverse_array' task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverse an array content
+ * @a: pointer integer parameter
+ * @n: pointer integer parameter
+ */
+void reverse_array(int *a, int n)
+{
+	int i, p;
+
+	for (i = 0; i < n--; i++)
+	{
+		p=a[i];
+		a[i]=a[n];
+		a[n]=p;
+	}
+}
