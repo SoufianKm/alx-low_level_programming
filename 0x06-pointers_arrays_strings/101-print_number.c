b55ba1bc1cdfd92fc3951149955340f8f714c1d4
SoufianKm <soufian.kamma@gmail.com>
2023-06-30 10:35:09 -0700

    Answer to 'Prints an integer' task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer
+ * @n : integer parameter
+ */
+void print_number(int n)
+{
+	int l;
+
+	l = n;
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	if (l / 10 != 0)
+	{
+		print_number(l / 10);
+	}
+	_putchar('0' + (l % 10))
+}
