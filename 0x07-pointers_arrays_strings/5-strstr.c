7ab99fd41dc37f750a5dc57a52d0841efb5d526f
SoufianKm <soufian.kamma@gmail.com>
2023-07-03 06:30:34 -0700

    Answer to 'strstr' task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - locate a substring 
+ * @haystack: char pointer parameter
+ * @needle: char pointer parameter
+ * Return: pointer to the beginning of the located
+ * substring, or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	s = strstr(haystack, needle);
+	return (s);
+}
