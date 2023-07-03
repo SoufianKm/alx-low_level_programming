31dd8b6d6c477959311bd54a2016aa6376c82215
SoufianKm <soufian.kamma@gmail.com>
2023-07-03 07:03:46 -0700

    Answer to 'Print chessboard' task

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - print the chess board
+ * @a: multidimensional array parameter
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j, l;
+
+	l = sizeof(a[0]);
+	for (i = 0; i < l; i++)
+	{
+		for (j = 0; j < l; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
