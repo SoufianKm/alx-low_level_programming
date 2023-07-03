d839ad6350a8af6bf1faceee19bbc90e35a455a4
SoufianKm <soufian.kamma@gmail.com>
2023-07-03 09:46:43 -0700

    Answer to 'strstr' task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 /**
  * _strstr - finds the first occurrence of the substring needle in the string haystack.
  * @haystack: char pointer parameter
  * @needle: char pointer parameter
- * Return: pointer to the beginning of the located
- * substring, NULL if the substring is not found.
+ * Return: pointer to the beginning of the located substring, NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
