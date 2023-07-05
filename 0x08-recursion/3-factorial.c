93fe32684575b18e976c1b7217b5b0f0bf5fa367
SoufianKm <soufian.kamma@gmail.com>
2023-07-05 13:43:30 -0700

    Answer to 'facturaial' task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - calculate the factorial of number
+ * @n : integer parameter
+ * Return: -1 if n lower than 0, 1 if equal to 0, else return the factorial of n.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
