f3a6a706842530b05c14d94625024223a58fc4cc
SoufianKm <soufian.kamma@gmail.com>
2023-07-06 10:48:29 -0700

    Answer to 'sqrt recursion' task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@
  */
 int calculate_sqrt(int i, int n)
 {
-	if (n < 0 || i < 0)
+	if (n < 0 || i > n)
 	{
 		return (-1);
 	} else if (n == 1)
@@ -17,12 +17,15 @@ int calculate_sqrt(int i, int n)
 		return (1);
 	}
 
-	if (i * i != n)
+	if (i * i == n)
 	{
-		i--;
+		return (i);
+	}
+	else
+	{
+		i++;
 		return (calculate_sqrt(i, n));
 	}
-	return (i);
 }
 
 /**
@@ -32,5 +35,5 @@ int calculate_sqrt(int i, int n)
  */
 int _sqrt_recursion(int n)
 {
-	return (calculate_sqrt(n - 1, n));
+	return (calculate_sqrt(1, n));
 }
