6b2c92139de7ee9cd30b5821e7e79379a9176925
SoufianKm <soufian.kamma@gmail.com>
2023-07-07 12:27:29 -0700

    Answer to 'Print the number of arguments' task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of array
+ * @argv: array of arguments
+ * Return: 0 always (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+
+	printf("%d\n", argc -1);
+
+	return (0);
+}
