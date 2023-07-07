db7cdf6101a4ab2c6b35064c27393c9fcbab51a5
SoufianKm <soufian.kamma@gmail.com>
2023-07-07 12:47:20 -0700

    Answer to 'Print all arguments' task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,16 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int count, i;
+	int i;
 
-	if (argc > 1)
-	{
-		count = argc - 1;
-		for (i = 0; i < count; i++)
-			printf("%s\n", argv[i]);
-	}
-	else
-		printf("%s\n", argv[0]);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
