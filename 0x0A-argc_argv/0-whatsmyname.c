624437173fd2a0f92e0a2654db9aab849c13f8a6
SoufianKm <soufian.kamma@gmail.com>
2023-07-07 12:16:47 -0700

    Answer to 'Print program name' task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of array
+ * @argv: array of arguments
+ * Return: 0 always (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
