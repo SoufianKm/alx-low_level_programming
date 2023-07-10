63bb856164aa46a54a3fcb897f021392638dc768
SoufianKm <soufian.kamma@gmail.com>
2023-07-10 08:25:04 -0700

    Answer to 'Creates an array of chars' task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *a;
 
-	a = malloc(sizeof(char) * size);
+	a = malloc(sizeof(char) * (size + 1));
 	if (a == NULL || size == 0)
 		return (NULL);
 
