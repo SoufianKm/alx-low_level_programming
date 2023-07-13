ceb36f378fa74efdf6d93db1a0f940a96a3cc59f
SoufianKm <soufian.kamma@gmail.com>
2023-07-13 09:05:36 -0700

    Answer to 'String nconcat' task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings from n number
+ * @s1: first string
+ * @s2: seconde string
+ * @n: number of characters from s2
+ * Return: NULL if s1 or s2 is NULL, else return the n string
+ * from s2 concatenated with s1
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *res;
+	int l = 0, i, j;
+
+	if (s1 == NULL || s2 == NULL)
+		return ('\0');
+
+	l += strlen(s1);
+	if (n <= strlen(s2))
+		l += strlen(s2);
+
+	res = malloc(sizeof(char) * (l + 1));
+
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+		res[i] = s1[i];
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		res[i] = s2[j];
+		i++;
+	}
+	res[i] = '\0';
+
+	return (res);
+}
