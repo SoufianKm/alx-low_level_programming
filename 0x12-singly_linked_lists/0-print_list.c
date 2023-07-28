2f56dd868b5c5cd385ac4f39c84ef6bf9516ec6d
SoufianKm <soufian.kamma@gmail.com>
2023-07-28 05:36:50 -0700

    Betty coding style resolved

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,14 +4,13 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
- *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while(h)
+	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
