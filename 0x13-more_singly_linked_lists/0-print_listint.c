923430f69962c3fd66eb3bfdace8826adba1b6e2
SoufianKm <soufian.kamma@gmail.com>
2023-07-31 03:30:44 -0700

    Answer to 'Print list int' task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to listint_t
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nbr = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nbr++;
+		h = h->next;
+	}
+
+	return (nbr);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+#This directory is about learning more slightly linked lists
+General
+- How to use linked lists
+- Start to look for the right source of information without too much help
