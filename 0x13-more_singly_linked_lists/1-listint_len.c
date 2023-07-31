3cda273c19eb96352d757bf0a2cc4e7da507d7fc
SoufianKm <soufian.kamma@gmail.com>
2023-07-31 03:39:00 -0700

    Answer to 'Listint len' task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t lists
+ * @h: linked list of type listint_t to traverse
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nbr = 0;
+
+	while (h)
+	{
+		nbr++;
+		h = h->next;
+	}
+
+	return (nbr);
+}
