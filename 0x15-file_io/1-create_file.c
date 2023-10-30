d6dd96adf908bd2ace3451f5d06cfb8c8e65f2dc
= <soufian.kamma@gmail.com>
2023-10-30 08:19:24 -0700

    Answer to seconde task 'Under the snow'

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success, -1 on failure or filename is NULL, an
+ * empty file if text_content is NULL
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, l = strlen(text_content);
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (0);
+
+	if (l)
+		bytes = write(fd, text_content, l);
+	close(fd);
+	return (bytes == l ? 1 : -1);
+}
