bf690d5783ed1ea0cb533a488bbc4e9fc53657f0
= <soufian.kamma@gmail.com>
2023-10-30 13:40:06 -0700

    Correct answer

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,45 +14,37 @@
  * Return: 1 on success, 0 on failuer
  */
 
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-int file_from, file_to;
-int num1 = READ_BUF_SIZE, num2 = 0;
-char buf[READ_BUF_SIZE];
+	int from_fd = 0, to_fd = 0;
+	ssize_t b;
+	char read_buf_size[READ_BUF_SIZE];
 
-if (argc != 3)
-	dprintf(STDERR_FILENO, USAGE), exit(97);
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-	dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
-	exit(98);
+	if (ac != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
+	from_fd = open(av[1], O_RDONLY);
+	if (from_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
+	while ((b = read(from_fd, read_buf_size, READ_BUF_SIZE)) > 0)
+		if (write(to_fd, read_buf_size, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
+	if (b == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
+	from_fd = close(from_fd);
+	to_fd = close(to_fd);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+
+	if (to_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+
+	return (0);
 }
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-if (file_to == -1)
-{
-	dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
-	close(file_from), exit(99);
-}
-while (num1 == 1024)
-{
-	num1 = read(file_from, buf, 1024);
-	if (num1 == -1)
-	{
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
-		exit(98);
-	}
-	num2 = write(file_to, buf, num1);
-	if (num2 < num1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
-}
-
-if (close(file_from) == -1)
-	dprintf(STDERR_FILENO, ERR_NOCLOSE, file_from), exit(100);
-
-if (close(file_to) == -1)
-	dprintf(STDERR_FILENO, ERR_NOCLOSE, file_to), exit(100);
-
-return (0);
-}
-
