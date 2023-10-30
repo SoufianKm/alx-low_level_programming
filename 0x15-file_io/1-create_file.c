0bf9e63ac414657ce71f7f6c7bb77690a812cf5e
= <soufian.kamma@gmail.com>
2023-10-30 08:21:03 -0700

    Correct the answer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,10 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes = 0, l = strlen(text_content);
 
 	if (!filename)
-		return (0);
+		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	if (l)
 		bytes = write(fd, text_content, l);
