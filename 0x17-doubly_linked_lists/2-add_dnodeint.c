4d1e26453a1610a22a56018178a8a246d6120cfd
= <soufian.kamma@gmail.com>
2023-12-07 05:24:59 -0800

    Task 2.Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = NULL;
+	dlistint_t *h = NULL, *new = NULL;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
@@ -17,9 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
-	if (*head)
-		(*head)->prev = new;
+	h = *head;
+	if (h != NULL)
+		while (h->prev != NULL)
+			h = h->prev;
+
+	new->next = h;
+	if (h != NULL)
+		h->prev = new;
+
 	*head = new;
 
 	return (new);
