92a252617469c8d0f59267840a52368e312a9347
= <soufian.kamma@gmail.com>
2023-12-22 09:03:39 -0800

    Task 2. key -> index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - return the index of a key.
+ * @key: the key
+ * @size: the size of the array of the hash table
+ *
+ * Return: the index at which the key/value pair should
+ * be stored in the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	if (size == 0 || key == NULL)
+		return (0);
+
+	hash = (hash_djb2(key) % size);
+
+	return (hash);
+}
