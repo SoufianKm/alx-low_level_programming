1c1cee63d8245f030485db05ec0f9cec06130c50
SoufianKm <soufian.kamma@com>
2024-04-09 07:49:39 -0700

    Interpolation search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ int binary_search(int *array, size_t size, int value);
 
 /* ADVANCED */
 int jump_search(int *, size_t, int);
+int interpolation_search(int *, size_t, int);
 
 #endif /* SEARCH_ALGOS_H */
